b3bf55aa33bda09efccfb57b0bee8b3ec9426f50
Owen Halbrook <33768369+odhalbrook@users.noreply.github.com>
2017-12-19 21:15:48 -0700

    Update ExitWorld.java

--- ExitWorld/src/ExitWorld.java
+++ ExitWorld/src/ExitWorld.java
@@ -4,6 +4,7 @@ public class ExitWorld {
 	public static void main(String[] args) {
 		System.out.println("Exit world, bye bye");
 		System.out.println("I'm leaving now");
+		System.out.println("I'm really leaving now");
 	}
 
 }
